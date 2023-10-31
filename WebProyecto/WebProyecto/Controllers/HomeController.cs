acfc3ff1bc055db5f924307003dae2232e83e319
arielguido <79162870+arielguido@users.noreply.github.com>
2023-10-31 14:18:08 -0600

    Commit  0

---
+++ WebProyecto/WebProyecto/Controllers/HomeController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Tarea_1.Models;
+
+namespace Tarea_1.Controllers
+{
+    public class HomeController : Controller
+    {
+        Usuario claseUsuario = new Usuario();
+
+        public ActionResult IniciarSesion()
+        {
+            return View();
+        }
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult Registrarse()
+        {
+            return View();
+        }
+
+
+
+        public ActionResult contraseña()
+        {
+            return View();
+        }
+
+
+
+    }
+}
