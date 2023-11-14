48256781d72ecbd155a420a040133d1c3c4fd264
BLOODBEL <101367583+BLOODBEL@users.noreply.github.com>
2023-11-14 10:17:59 -0600

    api

---
+++ APIProyecto/APIProyecto/Controllers/SalarioController.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace APIProyecto.Controllers
+{
+    public class SalarioController : Controller
+    {
+        [HttpGet]
+        [Route("ConsultaSalario")]
+        public List<Salario"> ConsultaSalario"()
+        {
+            try
+            {
+                using (var context = new ProyectoPAEntities())
+                {
+                    context.Configuration.LazyLoadingEnabled = false;
+                    return (from x in context.Salario
+                            select x).ToList();
+                }
+            }
+            catch (Exception)
+            {
+                return new List<Salario>();
+            }
+        }
+[HttpPut]
+[Route("ActualizarSalario)]
+public string ActualizarSalario(SalarioEnt entidad)
+{
+    try
+    {
+        using (var context = new ProyectoPAEntities())
+        {
+            context.ActualizarSalario(entidad.Salario, entidad.Descripcion);
+            return "OK";
+        }
+    }
+    catch (Exception)
+    {
+        return string.Empty;
+    }
+}
+
+    }
---
+++ APIProyecto/APIProyecto/Entities/SalarioEnt.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace APIProyecto.Entities
+{
+    public class SalarioEnt
+
+    {
+        public long IdSalario { get; set; }
+        public string Salario { get; set; }
+        public string Descripcion { get; set; }
+
+      
+    }
+}
